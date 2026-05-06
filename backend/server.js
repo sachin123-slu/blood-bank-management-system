27fd4c0aab3b615a9511b812eededc989e54da93
Sachin Kumar <devichampa487@gmail.com>
2026-05-06 15:06:54 +0530

    Updated CORS and production setup

--- backend/server.js
+++ backend/server.js
@@ -6,24 +6,29 @@ import authRoutes from "./routes/authRoutes.js";
 import donorRoutes from "./routes/donorRoutes.js";
 import adminRoutes from "./routes/adminRoutes.js";
 import facilityRoutes from "./routes/facilityRoutes.js";
-import { swaggerUi, swaggerDocs } from "./openapi/index.js"
+import { swaggerUi, swaggerDocs } from "./openapi/index.js";
 
 dotenv.config();
 const app = express();
 
 app.use(express.json());
 
-app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:5174"], // Allow both ports
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "https://bbm-system.vercel.app",
+    ], // Allow both ports
+    credentials: true,
+  }),
+);
 
-app.use('/api/doc', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.use("/api/doc", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 // 🧩 Routes
 
 app.use("/api/auth", authRoutes);
-
 
 app.use("/api/donor", donorRoutes);
 
@@ -31,18 +36,15 @@ app.use("/api/facility", facilityRoutes);
 
 app.use("/api/admin", adminRoutes);
 
-
-
 import bloodLabRoutes from "./routes/bloodLabRoutes.js";
 app.use("/api/blood-lab", bloodLabRoutes);
-
 
 import hospitalRoutes from "./routes/hospitalRoutes.js";
 app.use("/api/hospital", hospitalRoutes);
 
-
 // 🗄️ DB Connection
-const mongoUri = process.env.MONGO_URI || "mongodb://localhost:27017/blood_bank_db";
+const mongoUri =
+  process.env.MONGO_URI || "mongodb://localhost:27017/blood_bank_db";
 console.log("Connecting to MongoDB URI:", mongoUri);
 
 mongoose
